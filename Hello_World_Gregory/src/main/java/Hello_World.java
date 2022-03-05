a65eaeeeb72aaaa1eadc4cafa4175ca610670056
cgreg <cgreg@DESKTOP-IGOLB1M>
2022-03-04 23:46:38 -0600

    Initial commit.

---
+++ Hello_World_Gregory/src/main/java/Hello_World.java
@@ -0,0 +1,35 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello_World
+ */
+@WebServlet("/Hello_World")
+public class Hello_World extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Hello_World() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
